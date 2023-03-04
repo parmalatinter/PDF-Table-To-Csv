6ed574dbbb09047fe70eb69b282a9bf0176e2c6c
Yosuke Kobayashi <parmalatinter@gmail.com>
2023-03-04 15:59:20 +0900

    Update pdfToCsv.py

--- pdfToCsv.py
+++ pdfToCsv.py
@@ -13,21 +13,22 @@ baseDir = "../../確定申告/debit/"
 index1 = 0
 
 for i in range(2):
-	directory = baseDir + str(i + 1) + "/"
-	index2 = 0
-	for filename in os.listdir(directory):
-		if filename.endswith(".pdf"):
+    directory = baseDir + str(i + 1) + "/"
+    index2 = 0
+    for filename in os.listdir(directory):
+        if filename.endswith(".pdf"):
 
-			dfs = tabula.read_pdf(os.path.join(directory, filename), stream=True , pages = 'all', java_options="-Dfile.encoding=UTF8")
-			df = pd.concat(dfs, axis=0)
+            dfs = tabula.read_pdf(os.path.join(directory, filename), stream=True, pages='all',
+                                  java_options="-Dfile.encoding=UTF8")
+            df = pd.concat(dfs, axis=0)
 
-			if index1 + index2 == 0:
-				baseDf = df
-			else:
-				baseDf = pd.concat([baseDf, df[startRowIndex:]], axis=0)
-			index2 = index2 + 1
-		else:
-			continue
-	index1 = index1 + 1
-
-baseDf.to_csv( "result/result.csv", index=False, encoding='utf-8-sig')
+            if index1 + index2 == 0:
+                baseDf = df
+            else:
+                baseDf = pd.concat([baseDf, df[startRowIndex:]], axis=0)
+            index2 = index2 + 1
+        else:
+            continue
+    index1 = index1 + 1
+if not baseDf.empty:
+    baseDf.to_csv("result/result.csv", index=False, encoding='utf-8-sig')
