4bc74ab9579b2f210452c04697c477ae0aa95222
Yosuke Kobayashi <parmalatinter@gmail.com>
2023-07-28 18:18:12 +0900

    Update pdfToCsvGui.py

--- pdfToCsvGui.py
+++ pdfToCsvGui.py
@@ -1,38 +1,49 @@
-# PDF TO Excelアプリ
-# --------------------------
+# PDF TO CSVアプリ
+# exe アプリ化コマンド
+# pyinstaller gui.py --name pdf_to_csv.exe --onefile --noconsole
+
 import glob
 import tkinter as tk
 from tkinter import filedialog
+
 import pandas as pd
 import tabula
-import os
-
 
 class PdfToCsvApp(tk.Tk):
 
     def __init__(self):
         super().__init__()
-        self.title("PDF TO Excel")
+        self.title("PDF TO CSV")
         self.geometry("800x400")
 
         # ラベルとエントリーを配置するフレーム
-        input_frame = tk.Frame(self)
-        input_frame.pack(pady=10)
-        tk.Label(input_frame, text="フォルダ:").grid(row=0, column=0)
-        self.task_entry = tk.Entry(input_frame, width=30)
-        self.task_entry.grid(row=0, column=1)
+        input_directory_frame = tk.Frame(self)
+        input_directory_frame.pack(pady=10)
+        input_row_count_frame = tk.Frame(self)
+        input_row_count_frame.pack(pady=10)
+        tk.Label(input_directory_frame, text="フォルダ:").grid(row=0, column=0)
+        tk.Label(input_row_count_frame, text="ヘッダー数:").grid(row=0, column=0)
+        self.input_directory_value = tk.StringVar(value='')
+        self.directory_entry = tk.Entry(input_directory_frame, textvariable=self.input_directory_value, width=30)
+        self.directory_entry.grid(row=0, column=1)
+
+        # Entryウィジェットを読み取り専用にする
+        self.directory_entry.config(state="readonly")
+        self.header_row_count_entry = tk.Entry(input_row_count_frame, width=30)
+        self.header_row_count_entry.grid(row=0, column=1)
+        self.header_row_count_entry.insert(0, 1)
 
         # 追加ボタンを配置するフレーム
         btn_frame = tk.Frame(self)
         btn_frame.pack(pady=10)
 
         # タスクの一覧を表示するリストボックス
-        self.task_list = tk.Listbox(self, selectmode="multiple", bd=10)
+        self.task_list = tk.Listbox(self, bd=10)
         self.task_list.pack(fill="both", expand=True)
 
         # ボタンを作成してフォルダ選択関数を呼び出す
-        tk.Button(btn_frame, text="フォルダを選択", command=self.select_folder).pack(pady=10)
-        tk.Button(btn_frame, text="To CSV", command=self.to_csv).pack(side="left")
+        tk.Button(btn_frame, text="フォルダを選択", command=self.select_folder).pack(side="left")
+        tk.Button(btn_frame, text="To CSV", command=self.to_csv).pack(side="right")
 
         # タスクの一覧
         self.tasks = []
@@ -41,18 +52,26 @@ class PdfToCsvApp(tk.Tk):
     def add_log(self, msg):
         self.tasks.append(msg)
         self.task_list.insert("end", msg)
-        self.task_entry.delete(0, "end")
 
     # タスクの削除
     def delete_log(self):
         self.task_list.delete(0, tk.END)
 
-    # Todoアプリのクラス
+    # アプリのクラス
     def select_folder(self):
         folder_path = filedialog.askdirectory()  # フォルダ選択ダイアログを表示
         if folder_path:
-            print("選択したフォルダ:", folder_path)
-            self.task_entry.insert(0, folder_path)
+            self.input_directory_value.set(folder_path)  # 変数に新しい値をセット
+
+    def get_header_row_count_value(self):
+        input_value = self.header_row_count_entry.get()
+        try:
+            numeric_value = float(input_value)
+            print("入力された値:", numeric_value)
+            return int(input_value)
+        except ValueError:
+            print("有効な数値を入力してください。")
+            return 1
 
     def to_csv(self):
         ## 参考　https://bunkyudo.co.jp/python-tabula-01/
@@ -61,16 +80,16 @@ class PdfToCsvApp(tk.Tk):
         ## お取引日 お取引内容    ご利用額 利用手数料(円)現地通貨額 通貨 レート  Unnamed: 0  Unnamed: 1  確定
 
         base_df = None
-        start_row_index = 1
-        base_dir = self.task_entry.get()
+        base_dir = self.directory_entry.get()
         self.delete_log()
+        start_row_index = self.get_header_row_count_value()
+
         for filename in glob.glob(base_dir + '/*pdf'):
 
-
-            dfs = tabula.read_pdf(filename, stream=True, pages='all',
-                                  java_options="-Dfile.encoding=UTF8")
+            dfs = tabula.read_pdf(filename, stream=True, pages='all', java_options="-Dfile.encoding=UTF8")
             count = len(dfs)
             self.add_log(f'{filename} {count} 件')
+
             if len(dfs) == 0:
                 continue
 
@@ -84,7 +103,7 @@ class PdfToCsvApp(tk.Tk):
                 self.add_log(f"{base_dir}/result.csv にファイルを書き出しました")
 
 
-# PDF TO Excelアプリを起動する
+# アプリを起動する
 if __name__ == "__main__":
     app = PdfToCsvApp()
     app.mainloop()
